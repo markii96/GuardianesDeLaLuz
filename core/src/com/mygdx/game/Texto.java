180621d7d2b015fee5276f0fa337083fba8823bb
RICHO-PC\LUISRICARDO <ricardo.strifeff7@gmail.com>
2016-10-17 10:57:12 -0500

    Clase Texto y archivo de configuracion de heroes

---
+++ core/src/com/mygdx/game/Texto.java
@@ -0,0 +1,24 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by LUISRICARDO on 10/17/2016.
+ */
+public class Texto {
+    private BitmapFont font;
+
+    public Texto() {
+        font = new BitmapFont(Gdx.files.internal("tipo.fnt"));
+    }
+
+    public void mostrarMensaje(SpriteBatch batch, String mensaje, float x, float y) {
+        GlyphLayout glyp = new GlyphLayout();
+        glyp.setText(font, mensaje);
+        float anchoTexto = glyp.width;
+        font.draw(batch,glyp,x-anchoTexto/2,y);
+    }
+}
