8f640b457927d328a25baadfd5d76d95722305bb
marco <A01271519@itesm.mx>
2016-10-20 16:34:36 -0500

    aggregue la clase fondo guerrero y 2 imagenes nuevas

--- core/src/com/mygdx/game/Enemigo.java
+++ core/src/com/mygdx/game/Enemigo.java
@@ -1,8 +1,27 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 /**
  * Created by LUISRICARDO on 10/20/2016.
  */
 public class Enemigo {
+
+    private Estado estado;
+    private Sprite sprite;
+    private int[] posicionInicial = new int[2];
+
+    private String nombre;
+    private int danoFisico;
+    private int danoMagico;
+    private float alcance;
+    private float velocidadAtaque;
+    private int vitalidad;
+    private float velocidadMovimiento;
+    private int expSuelta;
+    private int nivelEncontrado;
+    private String descripcion;
+    private Texture textura;
 
 }
